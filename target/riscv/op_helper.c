76345b54f231a12ee4fdd0dfaad0f3debfdb4cc2
bigmagic <fujin.zhao@foxmail.com>
2021-06-16 14:47:12 +0800

    fixed config riscv linux user build err

--- target/riscv/op_helper.c
+++ target/riscv/op_helper.c
@@ -77,28 +77,34 @@ target_ulong helper_csrrc(CPURISCVState *env, target_ulong src,
 target_ulong nice_buf[3] = {0};
 void HELPER(lbuf)(target_ulong rs1)
 {
+#ifndef CONFIG_USER_ONLY
     cpu_physical_memory_rw(rs1, &nice_buf[0], 4, 0);
     cpu_physical_memory_rw(rs1 + 4, &nice_buf[1], 4, 0);
     cpu_physical_memory_rw(rs1 + 8,  &nice_buf[2], 4, 0);
+#endif
 }
 
 void HELPER(sbuf)(target_ulong rs1)
 {
+#ifndef CONFIG_USER_ONLY
     cpu_physical_memory_rw(rs1, &nice_buf[0], 4, 1);
     cpu_physical_memory_rw(rs1 + 4, &nice_buf[1], 4, 1);
     cpu_physical_memory_rw(rs1 + 8,  &nice_buf[2], 4, 1);
+#endif
 }
 
 target_ulong HELPER(rowsum)(target_ulong rs1)
 {
     target_ulong temp_buf[3] = {0};
 
+#ifndef CONFIG_USER_ONLY
     cpu_physical_memory_rw(rs1, &temp_buf[0], 4, 0);
     nice_buf[0] = nice_buf[0] + temp_buf[0];
     cpu_physical_memory_rw(rs1 + 4, &temp_buf[1], 4, 0);
     nice_buf[1] = nice_buf[1] + temp_buf[1];
     cpu_physical_memory_rw(rs1 + 8,  &temp_buf[2], 4, 0);
     nice_buf[2] = nice_buf[2] + temp_buf[2];
+#endif
 
     return temp_buf[0] + temp_buf[1] + temp_buf[2];
 }
