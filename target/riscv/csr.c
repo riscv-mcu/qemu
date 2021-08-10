6f30801f958340e4a03fc994fdf309bde431add6
bigmagic <fujin.zhao@foxmail.com>
2021-08-10 10:08:20 +0800

    fix qemu freq

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -375,11 +375,7 @@ static int read_instret(CPURISCVState *env, int csrno, target_ulong *val)
         }
         else
         {
-            //*val = cpu_get_host_ticks();
-            static int prev_instret = 0;
-            static int cnt = 0;
-            prev_instret = prev_instret + (cnt++);
-            *val = prev_instret;
+            *val = muldiv64(qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL), 32768, NANOSECONDS_PER_SECOND);
         }
     }
 #else
