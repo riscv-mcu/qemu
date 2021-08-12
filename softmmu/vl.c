8a01727e305c277ea130fdd55110bdbe134f66c5
bigmagic <fujin.zhao@foxmail.com>
2021-08-12 11:03:08 +0800

    Update version for nuclei-v1.0.3 release

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -843,7 +843,7 @@ static int machine_help_func(QemuOpts *opts, MachineState *machine)
     return 1;
 }
 
-#define NUCLEI_VERSION "nuclei-v1.0.2"
+#define NUCLEI_VERSION "nuclei-v1.0.3"
 static void version(void)
 {
     printf("QEMU emulator version " QEMU_FULL_VERSION "\n"
