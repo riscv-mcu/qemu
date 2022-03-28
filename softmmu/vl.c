b59f8344b1a1319f70a77f315c4af211156254f1
bigmagic <fujin.zhao@foxmail.com>
2022-03-28 16:17:42 +0800

    Releases 2022.04

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -843,7 +843,7 @@ static int machine_help_func(QemuOpts *opts, MachineState *machine)
     return 1;
 }
 
-#define NUCLEI_VERSION "nuclei-v1.0.6"
+#define NUCLEI_VERSION "2022.04"
 static void version(void)
 {
     printf("QEMU emulator version " QEMU_FULL_VERSION "\n"
