707ea046703af724051b35329491db92944199bc
Huaqi Fang <578567190@qq.com>
2022-08-16 15:10:38 +0800

    env: bump nuclei qemu version
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -843,7 +843,7 @@ static int machine_help_func(QemuOpts *opts, MachineState *machine)
     return 1;
 }
 
-#define NUCLEI_VERSION "2022.08"
+#define NUCLEI_VERSION "Nuclei QEMU 2022.08"
 static void version(void)
 {
     printf("QEMU emulator version " QEMU_FULL_VERSION "\n"
