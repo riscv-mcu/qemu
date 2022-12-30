c7115d84c9a2652c82c4458e09b6986da67a235a
lxx <lxx@qq.com>
2022-12-30 16:35:59 +0800

    target/riscv: Add minimal support for xxlcz extension

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -115,6 +115,7 @@ static const struct isa_ext_data isa_edata_arr[] = {
     ISA_EXT_DATA_ENTRY(svnapot, true, PRIV_VERSION_1_12_0, ext_svnapot),
     ISA_EXT_DATA_ENTRY(svpbmt, true, PRIV_VERSION_1_12_0, ext_svpbmt),
     ISA_EXT_DATA_ENTRY(xventanacondops, true, PRIV_VERSION_1_12_0, ext_XVentanaCondOps),
+    ISA_EXT_DATA_ENTRY(xxlcz, true, PRIV_VERSION_1_12_0, ext_xxlcz),
 };
 
 static bool isa_ext_is_enabled(RISCVCPU *cpu,
@@ -1304,6 +1305,10 @@ static Property riscv_cpu_extensions[] = {
     DEFINE_PROP_BOOL("x-zcf", RISCVCPU, cfg.ext_zcf, false),
     DEFINE_PROP_BOOL("x-zcmp", RISCVCPU, cfg.ext_zcmp, false),
     DEFINE_PROP_BOOL("x-zcmt", RISCVCPU, cfg.ext_zcmt, false),
+
+    /* Nuclei custom xxclz extension */
+    DEFINE_PROP_BOOL("x-xxlcz", RISCVCPU, cfg.ext_xxlcz, false),
+
     /* ePMP 0.9.3 */
     DEFINE_PROP_BOOL("x-epmp", RISCVCPU, cfg.epmp, false),
     DEFINE_PROP_BOOL("x-smaia", RISCVCPU, cfg.ext_smaia, false),
--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -567,6 +567,7 @@ struct RISCVCPUConfig {
     bool ext_smaia;
     bool ext_ssaia;
     bool ext_sscofpmf;
+    bool ext_xxlcz;
     bool rvv_ta_all_1s;
     bool rvv_ma_all_1s;
 
