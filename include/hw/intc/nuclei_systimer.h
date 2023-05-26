cc38aff00dc4f9a6d481e2ac09b26fa07011a174
bigmagic <fujin.zhao@foxmail.com>
2023-05-26 12:15:51 +0800

    hw/intc/nuclei_systimer: change evalsoc timer frequency to 32768

--- include/hw/intc/nuclei_systimer.h
+++ include/hw/intc/nuclei_systimer.h
@@ -74,7 +74,7 @@ typedef struct NucLeiSYSTIMERState
 #define  NUCLEI_N_TIMEBASE_FREQ     (32768)
 #define  NUCLEI_U_TIMEBASE_FREQ     (32768)
 #define  DEMOSOC_TIMEBASE_FREQ      (32768)
-#define  EVALSOC_TIMEBASE_FREQ      (1000000)//(32768)
+#define  EVALSOC_TIMEBASE_FREQ      (32768)
 
 enum {
     NUCLEI_SIP_BASE     = 0x1000,
