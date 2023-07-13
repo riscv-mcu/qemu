86380d8eeaec686c302565b2cd71979df2fe70fd
bigmagic <fujin.zhao@foxmail.com>
2023-07-13 17:54:17 +0800

    target/riscv: fixed wfi exected while eclic irq panding

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -695,11 +695,22 @@ static bool riscv_cpu_has_work(CPUState *cs)
 #ifndef CONFIG_USER_ONLY
     RISCVCPU *cpu = RISCV_CPU(cs);
     CPURISCVState *env = &cpu->env;
+
+    int eclic_irq_panding = env->exccode & 0x3FF;
     /*
      * Definition of the WFI instruction requires it to ignore the privilege
      * mode and delegation registers, but respect individual enables
      */
-    return riscv_cpu_all_pending(env) != 0;
+    if((eclic_irq_panding > 0) && (eclic_irq_panding < 0x3ff))
+    {
+
+    }
+    else
+    {
+        eclic_irq_panding = 0;
+    }
+
+    return (riscv_cpu_all_pending(env) != 0) || eclic_irq_panding;
 #else
     return true;
 #endif
