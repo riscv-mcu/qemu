bf1ec85407c8a769987d0c8be1219de6d073ffa7
Deepak Gupta <debug@rivosinc.com>
2024-10-08 15:50:00 -0700

    target/riscv: Add zicfiss extension
    zicfiss [1] riscv cpu extension enables backward control flow integrity.
    
    This patch sets up space for zicfiss extension in cpuconfig. And imple-
    ments dependency on A, zicsr, zimop and zcmop extensions.
    
    [1] - https://github.com/riscv/riscv-cfi
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Co-developed-by: Jim Shu <jim.shu@sifive.com>
    Co-developed-by: Andy Chiu <andy.chiu@sifive.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-ID: <20241008225010.1861630-11-debug@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/cpu_cfg.h
+++ target/riscv/cpu_cfg.h
@@ -69,6 +69,7 @@ struct RISCVCPUConfig {
     bool ext_zicbop;
     bool ext_zicboz;
     bool ext_zicfilp;
+    bool ext_zicfiss;
     bool ext_zicond;
     bool ext_zihintntl;
     bool ext_zihintpause;
