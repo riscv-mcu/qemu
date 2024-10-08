086aa08f9dbbea9eaae357bb83e677ad4882e17d
Deepak Gupta <debug@rivosinc.com>
2024-10-08 15:50:06 -0700

    target/riscv: implement zicfiss instructions
    zicfiss has following instructions
     - sspopchk: pops a value from shadow stack and compares with x1/x5.
       If they dont match, reports a sw check exception with tval = 3.
     - sspush: pushes value in x1/x5 on shadow stack
     - ssrdp: reads current shadow stack
     - ssamoswap: swaps contents of shadow stack atomically
    
    sspopchk/sspush/ssrdp default to zimop if zimop implemented and SSE=0
    
    If SSE=0, ssamoswap is illegal instruction exception.
    
    This patch implements shadow stack operations for qemu-user and shadow
    stack is not protected.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Co-developed-by: Jim Shu <jim.shu@sifive.com>
    Co-developed-by: Andy Chiu <andy.chiu@sifive.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-ID: <20241008225010.1861630-17-debug@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -144,6 +144,8 @@ static inline bool has_ext(DisasContext *ctx, uint32_t ext)
 #define get_address_xl(ctx)    ((ctx)->address_xl)
 #endif
 
+#define mxl_memop(ctx) ((get_xl(ctx) + 1) | MO_TE)
+
 /* The word size for this machine mode. */
 static inline int __attribute__((unused)) get_xlen(DisasContext *ctx)
 {
@@ -1105,6 +1107,8 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
     return cpu_ldl_code(env, pc);
 }
 
+#define SS_MMU_INDEX(ctx) (ctx->mem_idx | MMU_IDX_SS_WRITE)
+
 /* Include insn module translation function */
 #include "insn_trans/trans_rvi.c.inc"
 #include "insn_trans/trans_rvm.c.inc"
@@ -1136,6 +1140,7 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 #include "insn_trans/trans_zilsd.c.inc"
 #include "insn_trans/trans_rvzibi.c.inc"
 #include "insn_trans/trans_rvzcmop.c.inc"
+#include "insn_trans/trans_rvzicfiss.c.inc"
 
 /* Include decoders for factored-out extensions */
 #include "decode-XVentanaCondOps.c.inc"
