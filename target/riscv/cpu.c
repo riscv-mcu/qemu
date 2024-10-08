694f4667fc6329cb3633ad073a2c6705de2eed64
Deepak Gupta <debug@rivosinc.com>
2024-10-08 15:49:51 -0700

    target/riscv: expose *envcfg csr and priv to qemu-user as well
    Execution environment config CSR controlling user env and current
    privilege state shouldn't be limited to qemu-system only. *envcfg
    CSRs control enabling of features in next lesser mode. In some cases
    bits *envcfg CSR can be lit up by kernel as part of kernel policy or
    software (user app) can choose to opt-in by issuing a system call
    (e.g. prctl). In case of qemu-user, it should be no different because
    qemu is providing underlying execution environment facility and thus
    either should provide some default value in *envcfg CSRs or react to
    system calls (prctls) initiated from application. priv is set to PRV_U
    and menvcfg/senvcfg set to 0 for qemu-user on reest.
    
    `henvcfg` has been left for qemu-system only because it is not expected
    that someone will use qemu-user where application is expected to have
    hypervisor underneath which is controlling its execution environment. If
    such a need arises then `henvcfg` could be exposed as well.
    
    Relevant discussion:
    https://lore.kernel.org/all/CAKmqyKOTVWPFep2msTQVdUmJErkH+bqCcKEQ4hAnyDFPdWKe0Q@mail.gmail.com/
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Suggested-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-ID: <20241008225010.1861630-2-debug@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1409,7 +1409,12 @@ static void riscv_cpu_reset_hold(Object *obj)
     }
 
     pmp_unlock_entries(env);
+#else
+    env->priv = PRV_U;
+    env->senvcfg = 0;
+    env->menvcfg = 0;
 #endif
+
     env->xl = riscv_cpu_mxl(env);
     riscv_cpu_update_mask(env);
     cs->exception_index = RISCV_EXCP_NONE;
