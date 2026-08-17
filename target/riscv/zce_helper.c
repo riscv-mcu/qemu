35050cc23b211fa1d7de4a8809a5c393acb3fd81
lxx <1733205434@qq.com>
2026-08-17 09:56:22 +0800

    target/riscv: Add big-endian support for Zcmt extension

--- target/riscv/zce_helper.c
+++ target/riscv/zce_helper.c
@@ -18,12 +18,16 @@
 
 #include "qemu/osdep.h"
 #include "cpu.h"
+#include "internals.h"
 #include "exec/exec-all.h"
 #include "exec/helper-proto.h"
 #include "exec/cpu_ldst.h"
 
 target_ulong HELPER(cm_jalt)(CPURISCVState *env, uint32_t index)
 {
+    int mmu_idx = cpu_mmu_index(env_cpu(env), true);
+    MemOp endian = mo_endian_env(env);
+    MemOpIdx oi;
 
 #if !defined(CONFIG_USER_ONLY)
     RISCVException ret = smstateen_acc_ok(env, 0, SMSTATEEN0_JVT);
@@ -44,11 +48,13 @@ target_ulong HELPER(cm_jalt)(CPURISCVState *env, uint32_t index)
     }
 
     if (xlen == 32) {
+        oi = make_memop_idx(MO_UL | endian, mmu_idx);
         t0 = base + (index << 2);
-        target = cpu_ldl_code(env, t0);
+        target = cpu_ldl_code_mmu(env, t0, oi, 0);
     } else {
+        oi = make_memop_idx(MO_UQ | endian, mmu_idx);
         t0 = base + (index << 3);
-        target = cpu_ldq_code(env, t0);
+        target = cpu_ldq_code_mmu(env, t0, oi, 0);
     }
 
     return target & ~0x1;
